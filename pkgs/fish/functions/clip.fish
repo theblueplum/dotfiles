f1b828c7fefdcf8e84ba9270e1a8d4f932673c24
Anton <aligator.h0spital.e@gmail.com>
2025-12-19 16:06:43 +0100

    chore: standardize fish config (+ sync `into`)

--- pkgs/fish/functions/clip.fish
+++ pkgs/fish/functions/clip.fish
@@ -1,26 +1,19 @@
 function clip
-    if not command -q xclip
-		echoerr "clip: 'xclip' is not available on this system"
+	if not command -q xclip
+		echoerr "clip: xclip is not available on this system"
 		return 1
 	end
 
 	if not tty -s
-		set -l stdin
-		read stdin -z
+		read -zl stdin
+		echo $stdin | xclip -selection clipboard
+		return $status
 	end
 
-	if set -ql stdin
-		echo "$stdin" | xclip -selection clipboard
-		echo "copied into clipboard (stdin)"
-		return 0
-	end
-
-	if [ -n "$(string trim -- $argv)" ]
-		echo "$argv" | xclip -selection clipboard
-		echo "copied into clipboard"
-		return 0
+	if string length -qV -- (string trim -- $argv)
+		echo $argv | xclip -selection clipboard
+		return $status
 	end
 
 	xclip -selection clipboard -o
 end
-
---
+++ pkgs/fish/functions/into.fish
@@ -0,0 +1,4 @@
+function into -a dir
+	mkdir -p $dir
+	cd $dir
+end
